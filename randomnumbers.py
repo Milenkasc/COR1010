3e54b77e8ab206d5e77709b7c186d16dde51b76a
Milenkasc <163966777+Milenkasc@users.noreply.github.com>
2024-03-19 13:43:09 +0900

    Add files via upload

---
+++ randomnumbers.py
@@ -0,0 +1,14 @@
+#random numbers!
+
+import random 
+import numpy as np
+#pip install numpy
+#pip install numpy for Mac
+
+number = random.randint (1,20)
+print (number)
+
+#npunumber = np.randm.
+#generate 100 random numbers 
+#put them in a list 
+#print
